620e3edade42bd6e080e6d7822d0371252120faf
Ben <ben.bromhead@gmail.com>
2021-03-18 14:56:42 +1300

    Build windows binary with .exe extension

--- agent/cross_compile.sh
+++ agent/cross_compile.sh
@@ -3,10 +3,11 @@ operatingSystems=("darwin" "linux" "windows")
 
 for os in ${operatingSystems[@]}; do
 	echo "Building $os for amd64"
-	GOOS="${os}" GOARCH="amd64" go build -o "agent_${os}_amd64"
 	if [[ $os = "windows" ]]; then
-		zip "agent_${os}_amd64.zip" "agent_${os}_amd64"
+		GOOS="${os}" GOARCH="amd64" go build -o "agent_${os}_amd64.exe"
+		zip "agent_${os}_amd64.zip" "agent_${os}_amd64.exe"
 	else
+	        GOOS="${os}" GOARCH="amd64" go build -o "agent_${os}_amd64"
 		tar -czvf "agent_${os}_amd64.tar.gz" "agent_${os}_amd64"
 	fi
 #	rm "agent_${os}_amd64"
